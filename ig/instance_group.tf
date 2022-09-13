e4c073e8de11dd6a197effcccd23e9bae2f70e58
mvn-linhnguyen7-hn <linh.nguyen7@monstar-lab.com>
2022-09-13 08:52:24 +0700

    update ig

--- ig/instance_group.tf
+++ ig/instance_group.tf
@@ -1,9 +1,11 @@
 
-resource "google_compute_instance_group_manager" "appserver" {
+resource "google_compute_region_instance_group_manager" "appserver" {
   name = "appserver-igm"
 
   base_instance_name = "app"
-  zone     = "europe-west1-c"
+  # zone     = "europe-west1-c"
+  region                     = "europe-west1"
+  distribution_policy_zones  = ["europe-west1-a", "europe-west1-c"]
 
   named_port {
     name = "http"
